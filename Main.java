4f3e62cd90c04cb13cee4624843d6939e591abd0
Dinansa Rithmini <dinarithmi123@gmail.com>
2024-02-20 14:38:04 +0530

    commit5

--- Main.java
+++ Main.java
@@ -27,14 +27,16 @@ class Parcel {
         String location;
         int distance;
 
+        int priority;
 // Constructor for Courier class
-        public Courier(String parcelId, String courier_id, int distance, double deliveryfee, String location, double weight) {
+        public Courier(String parcelId, String courier_id, int distance, double deliveryfee, String location, double weight , int priority) {
             this.parcelId = parcelId;
             this.courier_id = courier_id;
             this.distance = distance;
             this.deliveryfee = deliveryfee;
             this.location = location;
             this.weight = weight;
+            this.priority=priority;
         }
     }
         class Node {
@@ -67,6 +69,45 @@ class Parcel {
                     newNode.prev = current;
                 }
             }
+            public void sort() {
+                boolean sorted = false;
+                while (!sorted) {
+                    sorted = true;
+                    Node current = head;
+                    Node previous = null;
+                    while (current != null && current.next != null) {
+                        if (current.courier.priority > current.next.courier.priority) {
+                            if (previous == null) {
+                                head = current.next;
+                            } else {
+                                previous.next = current.next;
+                            }
+                            Node temp = current.next;
+                            current.next = temp.next;
+                            temp.next = current;
+                            previous = temp;
+                            sorted = false;
+                        } else {
+                            previous = current;
+                            current = current.next;
+                        }
+                    }
+                }
+            }
+
+            public void displaySorted()
+            {
+                Node current = head;
+                System.out.println("Sorted List based on Priority:");
+                while (current != null) {
+                    System.out.println("Parcel ID: " + current.parcel.parcelId);
+                    System.out.println("Destination: " + current.courier.location);
+                    System.out.println("Priority: " + current.courier.priority);
+                    System.out.println("");
+                    current = current.next;
+                }
+            }
+
 // Method to display Parcel details
             public void displayparseldetails() {
                 Node current = head;
@@ -90,6 +131,7 @@ class Parcel {
                     System.out.println("Distance: " + current.courier.distance);
                     System.out.println("Weight: " + current.courier.weight);
                     System.out.println("Location: " + current.courier.location);
+                    System.out.println("Priority"+ current.courier.priority);
                     System.out.println("Delivery  Fee: " + current.courier.deliveryfee);
 
                     current = current.next;
@@ -110,46 +152,52 @@ class Parcel {
                  System.out.println("1.Add a parcel and courier");
                  System.out.println("2.Display parcel details");
                  System.out.println("3.Display courier details");
-                 System.out.println("4.Exit");
-                 scanner.nextLine();
+                 System.out.println("4.Display the priorities of the parcels");
+                 System.out.println("5.Exit");
+
                  System.out.println("Input your choice");
 
-                    //Reading user choice
+                 //Reading user choice
                  int choice = scanner.nextInt();
                  scanner.nextLine();
-                    //Switch cases for user choices
+                 //Switch cases for user choices
                     switch (choice) {
                         //If the user choose the option 01) add a parcel and courier
                         case 1:
-                            System.out.println("Enter parcel ID:");
-                            String parcelId = scanner.nextLine();
+                            for(int i=0; i<=4; i++) {
+                                System.out.println("Enter parcel ID:");
+                                String parcelId = scanner.nextLine();
 
-                            System.out.println("Enter sender's name:");
-                            String senderName = scanner.nextLine();
-                            System.out.println("Enter recipient's name:");
-                            String recipientName = scanner.nextLine();
-                            System.out.println("Enter parcel price:Rs.");
-                            double price = scanner.nextDouble();
-                            System.out.println("Enter distance: Km");
-                            int distance = scanner.nextInt();
-                            System.out.println("Enter weight: Kg");
-                            int weight = scanner.nextInt();
-                            scanner.nextLine(); //Newline
-                            System.out.println("Enter courier ID:");
-                            String courierId = scanner.nextLine();
-                            System.out.println("Enter courier location:");
-                            String location = scanner.nextLine();
-                            System.out.println("Enter courier delivery fee: Rs");
-                            double deliveryFee = scanner.nextDouble();
-                            System.out.println("Enter courier weight: Kg");
-                            double courierWeight = scanner.nextDouble();
-                            scanner.nextLine(); //Newline
-                            //Creating Parcel and Courier objects
-                            Parcel parcel = new Parcel(parcelId, senderName, recipientName, price,  weight);
-                            Courier courier = new Courier(parcelId, courierId, distance, deliveryFee, location, courierWeight);
-                           //Inserting into the doubly linked list
-                            dll.insert(parcel, courier);
-
+                                System.out.println("Enter sender's name:");
+                                String senderName = scanner.next();
+                                senderName = scanner.nextLine();
+                                System.out.println("Enter recipient's name:");
+                                String recipientName = scanner.next();
+                                recipientName = scanner.nextLine();
+                                System.out.println("Enter parcel price:");
+                                double price = scanner.nextDouble();
+                                System.out.println("Enter distance: ");
+                                int distance = scanner.nextInt();
+                                System.out.println("Enter weight: ");
+                                int weight = scanner.nextInt();
+                                scanner.nextLine(); //Newline
+                                System.out.println("Enter courier ID:");
+                                String courierId = scanner.nextLine();
+                                System.out.println("Enter courier location:");
+                                String location = scanner.nextLine();
+                                System.out.println("Enter courier delivery fee: ");
+                                double deliveryFee = scanner.nextDouble();
+                                System.out.println("Enter courier weight: ");
+                                double courierWeight = scanner.nextDouble();
+                                System.out.println("Enter courier priority: ");
+                                int priority = scanner.nextInt();
+                                scanner.nextLine(); //Newline
+                                //Creating Parcel and Courier objects
+                                Parcel parcel = new Parcel(parcelId, senderName, recipientName, price, weight);
+                                Courier courier = new Courier(parcelId, courierId, distance, deliveryFee, location, courierWeight, priority);
+                                //Inserting into the doubly linked list
+                                dll.insert(parcel, courier);
+                            }
                             break;
 
                         case 2:
@@ -165,13 +213,15 @@ class Parcel {
                             dll.displaycourierdetails();
 
                             break;
-
                         case 4:
+                            System.out.println("Priority listing");
+                            dll.sort();
+                            dll.displaySorted();
+                             break;
+                        case 5:
                             //If the user choose the option 04) Exit
-                            System.out.println("Exiting...");
-                            System.exit(0);
-
-                        default:
+                            System.out.println("Exit Application");
+                            default:
                             //If the user choose invalid index
                             System.out.println("Invalid choice. Please select correct  numeric value.");
                 }
